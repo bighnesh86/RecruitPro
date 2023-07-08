47667de39043ceb4864f6484cadcb87ff9f876fa
Bighnesh Panda <bighnesh0456@gmail.com>
2023-07-08 07:44:28 +0530

    update: added some customizations

--- login/script.js
+++ login/script.js
@@ -1,29 +1,56 @@
-// Login form submit event
-document.getElementById('loginForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    // Perform login logic here
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    // Example login validation
-    if (username === 'admin' && password === 'admin123') {
-      alert('Login successful');
-      // Redirect to the homepage or desired page
-      window.location.href = 'candidate-search.html';
-    } else {
-      alert('Invalid username or password');
-    }
-  });
+// Login form
+const loginForm = document.getElementById('loginForm');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Get input values
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  // Check if user exists in localStorage
+  const userData = JSON.parse(localStorage.getItem('userData'));
+  if (userData && userData.username === username && userData.password === password) {
+    // Successful login
+    alert('Login successful');
+    // Redirect to the dashboard or desired page
+    window.location.href = 'candidate-search.html';
+  } else {
+    // Invalid credentials
+    alert('Invalid username or password');
+  }
+
+  // Clear form fields
+  loginForm.reset();
+});
+
+// Sign up form
+const signupForm = document.getElementById('signupForm');
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Get input values
+  const fullName = document.getElementById('fullName').value;
+  const email = document.getElementById('email').value;
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  // Create user object
+  const userData = {
+    fullName,
+    email,
+    username,
+    password
+  };
+
+  // Store user data in localStorage
+  localStorage.setItem('userData', JSON.stringify(userData));
+
+  // Successful sign-up
+  alert('Sign up successful');
   
-  // Register form submit event
-  document.getElementById('registerForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    // Perform registration logic here
-    var companyName = document.getElementById('companyName').value;
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    // Example registration logic
-    alert('Registration successful');
-    // Redirect to the login page or desired page
-    window.location.href = 'login.html';
-  });
-  
+  // Redirect to the login page
+  window.location.href = 'login.html';
+
+  // Clear form fields
+  signupForm.reset();
+});
